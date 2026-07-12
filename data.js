3084f4a68bd421be7d8d3f6cbdb60c67fa1c96ff
Black-Echo1 <black.echore@gmail.com>
2026-07-12 21:02:59 +0000

    i updated the website to include new features and fix some bugs. The changes include updates to the data.js file, modifications to the index.html, legal.html

--- data.js
+++ data.js
@@ -4,17 +4,17 @@ const videosData = {
     men: [
         { 
             title: "مشهد Death Note - الحلقة الأولى", 
-            thumbnail: "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?q=80&w=600&auto=format&fit=crop", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWpzOjp6FqK0IGEPnVMOaBzzlko_ti3bTeRxmCCSFozwCxiR4Trhef2B8&s=10", 
             driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
         },
         { 
             title: "مشهد Kill Ao - أكشن وتحدي", 
-            thumbnail: "https://images.unsplash.com/photo-1542204165-65bf26472b9b?q=80&w=600&auto=format&fit=crop", 
+            thumbnail: "https://anitousen.com/uploads/posts/2026-04/3af4701457_anitousen-kill-ao-2026.jpg", 
             driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
         },
         { 
             title: "مشهد Kill Ao - أكشن وتحدي", 
-            thumbnail: "https://images.unsplash.com/photo-1542204165-65bf26472b9b?q=80&w=600&auto=format&fit=crop", 
+            thumbnail: "https://talkpod.com/cdn/shop/articles/which-demon-slayer-personality-wise-scares-intimidates-you-v0-ab4zw65wy7gb1.webp?v=1739345805&width=2048", 
             driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
         }
     ],
@@ -23,6 +23,19 @@ const videosData = {
         { 
             title: "مشهد Demon Slayer - دراما", 
             thumbnail: "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=600&auto=format&fit=crop", 
+            driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
+        }
+         ,
+     
+        { 
+            title: "مشهد Demon Slayer - دراما", 
+            thumbnail: "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=600&auto=format&fit=crop", 
+            driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
+        },
+     
+        { 
+            title: "مشهد Demon Slayer - دراما", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS266APKGEReOJtHJxODb_F9YmLdkxsfl51FumRH53nxvzRSc9th186Efc&s=10", 
             driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
         }
     ]
