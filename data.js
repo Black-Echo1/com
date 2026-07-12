88b277960ddfd0143b84e7d72597a377bd9ec358
Black-Echo1 <black.echore@gmail.com>
2026-07-12 22:16:39 +0000

    fix main page

--- data.js
+++ data.js
@@ -3,40 +3,40 @@ const videosData = {
     // قسم الشباب
     men: [
         { 
-            title: "مشهد Death Note - الحلقة الأولى", 
-            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWpzOjp6FqK0IGEPnVMOaBzzlko_ti3bTeRxmCCSFozwCxiR4Trhef2B8&s=10", 
-            driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
+            title: "الحقيقة joker fire force dark hero", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9jDvqdw5SEPyvunvPCZ3piyVT6O_E4h7byvu4b3aeKw&s=10", 
+            driveId: "12oyCeTdeV9Y953CMJXcOdRMe7Rn0PIPy" 
         },
         { 
-            title: "مشهد Kill Ao - أكشن وتحدي", 
-            thumbnail: "https://anitousen.com/uploads/posts/2026-04/3af4701457_anitousen-kill-ao-2026.jpg", 
-            driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
+            title: "bleach aizen ايزن", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwDwH2jdLsunF2ZT_6MnLwOLc09yPKGSNe3y4GUCeR7w&s", 
+            driveId: "11BPLzVA1e23zvuNrF6NOpzsd4_yZAUBm" 
         },
         { 
-            title: "مشهد Kill Ao - أكشن وتحدي", 
-            thumbnail: "https://talkpod.com/cdn/shop/articles/which-demon-slayer-personality-wise-scares-intimidates-you-v0-ab4zw65wy7gb1.webp?v=1739345805&width=2048", 
-            driveId: "1k9b0sYyCKyprYlc_vycuGK8NQlXvhYZm" 
+            title: "solo leveling - سولو ليفلينج", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Y4tl_qPCnU0tEAFJFMRr-hGKzuE07k8gb3i08KjWiA&s=10", 
+            driveId: "1RYEhExhTJnKfChakO-bOg8evEg5C8G2b" 
         }
     ],
     // قسم الفتيات
     women: [
         { 
-            title: "مشهد Demon Slayer - دراما", 
-            thumbnail: "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=600&auto=format&fit=crop", 
-            driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
+            title: "Spy Family - دراما", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiULh0eZ9jyJVwgicN3bWeYbRu-1HKo7mczzC8fCDbnSUss3z0LuweVLjT&s=10", 
+            driveId: "1hrR_-DojIRj0BwgvqBrpZ7EDiuo3a4j8" 
         }
          ,
      
         { 
-            title: "مشهد Demon Slayer - دراما", 
-            thumbnail: "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=600&auto=format&fit=crop", 
-            driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
+            title: "shield hero مشاعر وا امتينان", 
+            thumbnail: "https://miro.medium.com/v2/resize:fit:1200/1*HYQCQYjCdXQ0tjM8Oph_wQ.jpeg", 
+            driveId: "1DSP2DkbTRhgmmqJ7p9L_PmFqj6tll6rp" 
         },
      
         { 
-            title: "مشهد Demon Slayer - دراما", 
-            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS266APKGEReOJtHJxODb_F9YmLdkxsfl51FumRH53nxvzRSc9th186Efc&s=10", 
-            driveId: "ضع_الـID_الخاص_بجوجل_درايف_هنا" 
+            title: "مشهد بل مؤثرات فقط", 
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAyi-qR5sIo_Xw4fl6IW2lORWjziu-bWWvgnszhBMoGA&s=10", 
+            driveId: "125FQDtEHbe3_JwYU1Z-CUD4Qi68p9c1T" 
         }
     ]
 };
