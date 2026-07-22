29588b538b7e8e9f7133bb09e91105109784e620
Black-Echo1 <black.echore@gmail.com>
2026-07-22 08:45:52 +0000

    i fix image

--- data/dubbers_data.js
+++ data/dubbers_data.js
@@ -2,16 +2,16 @@ const dubbersDatabase = {
     "أحمد منصور": {
         name: "أحمد منصور",
         role: "مؤدي أصوات ",
-        logo: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/43fdd59cca083733b9a5f418954b2258~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=8723b95d&x-expires=1784808000&x-signature=tznw6s%2Fu2b52KUqxDZmD85Yru7M%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my3", // <--- أضف هذا الرابط إن أردت
+        logo: "https://files.catbox.moe/zxvbr4.jpeg", // <--- أضف هذا الرابط إن أردت
         roles: [
-           { charName: "كوتوها", animeTitle: "wind breaker", animeId: "wind breaker", charImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYLXNlGGMuSXSHv9a1P59-wxOxq64PnIC395rrnmjNmZWyYcuzuByPrMo&s=10" }
+           { charName: "Hiiragi, Touma", animeTitle: "wind breaker", animeId: "wind breaker", charImage: "https://cdn.myanimelist.net/images/characters/7/467088.jpg" }
         ]
     },
 
     "mohamed.arata": {
         name: "mohamed.arata",
         role: "مؤدية أصوات",
-        logo: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/b08c620c18d2a930b80db342d9beae66~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=4e49b23b&x-expires=1784815200&x-signature=ghvmE53VjWZQ%2FfrCR8m%2FjfPAalY%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my3",
+        logo: "https://files.catbox.moe/90nptg.jpeg",
         roles: [
             { charName: "Erwin Smith ", animeTitle: "Attack on Titan: No Regrets", animeId: "Attack on Titan: No Regrets", charImage: "https://cdn.myanimelist.net/images/characters/14/559023.jpg" }
         ]
@@ -19,7 +19,7 @@ const dubbersDatabase = {
     "zinkomah": {
         name: "عبد الرزاق",
         role: "مؤدي أصوات / مخرج/ قائد الفريق / مكساج",
-        logo: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/d483feb65a1743e6bfccf2e66180a0e8~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=6aa54610&x-expires=1784818800&x-signature=VN0zt984l3GFz3BfGkoA6BUyKH8%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my3",
+        logo: "https://files.catbox.moe/0t13u4.jpeg",
         roles: [
             { charName: "Church, Farlan", animeTitle: "Attack on Titan: No Regrets", animeId: "Attack on Titan: No Regrets", charImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRviOTfgvVONHd5v1WgvKE5_Wh_j2jWec4ik0j6zBvPrQ&s=10" },
              { charName: "sakuraSakura, Haruka", animeTitle: "wind breaker", animeId: "wind breaker", charImage: "https://cdn.myanimelist.net/images/characters/9/458760.jpg" }
@@ -29,7 +29,7 @@ const dubbersDatabase = {
         name: "fatomadub1",
          role: "مؤدي أصوات ", logo: "",
           roles: [
-         { charName: "", animeTitle: "", animeId: "", charImage: "" } ] }
+          ] }
 ,
     "asmaa9.esmael_adm6": { 
         name: "أسماء إسماعيل آدم",
@@ -39,20 +39,20 @@ const dubbersDatabase = {
     "sweet.angel8001": { 
         name: "sweet.ange",
          role: "مؤدي أصوات ", 
-         logo: "",
+         logo: "https://files.catbox.moe/daxnzz.jpeg",
           roles: [
          { charName: "", animeTitle: "", animeId: "", charImage: "" } ] },
     "mohamed akram": { 
         name: "mohamed akram",
          role: "مؤدي أصوات ",
-          logo: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/c1a508a0e337b11ff0db8066e504d141~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=6dc0af2d&x-expires=1784818800&x-signature=LAyRdkFYTELIUHQyMrEi8Syf2yc%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my3",
+          logo: "https://files.catbox.moe/xbqcrg.jpeg",
           roles: [
          { charName: "Suou, Hayato", animeTitle: "Wind breaker", animeId: "wind breaker", charImage: "https://cdn.myanimelist.net/images/characters/2/458759.jpg" } ] },
 
     "makoto_san00": {
         name: "ماكوتو_سان | Makoto_san",
         role: "مؤدي أصوات",
-        logo: "https://p19-common-sign.tiktokcdn-us.com/tos-alisg-avt-0068/7fe345cae7da448b4407044f15101e7b~tplv-tiktokx-cropcenter:1080:1080.webp?dr=9640&refresh_token=c80ae5b0&x-expires=1784836800&x-signature=0XvLDOwJJ43elpP763RtWZYB4Hc%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=2472a6c6&idc=useast8",
+        logo: "https://files.catbox.moe/4neejs.webp",
         roles: [
            { charName: "كوتوها", animeTitle: "wind breaker", animeId: "wind breaker", charImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYLXNlGGMuSXSHv9a1P59-wxOxq64PnIC395rrnmjNmZWyYcuzuByPrMo&s=10" }
         ]
@@ -60,27 +60,35 @@ const dubbersDatabase = {
     "كنجي": { 
         name: "كنجي",
          role: "مؤدي أصوات ",
-          logo: "",
+          logo: "https://files.catbox.moe/91wlmm.jpeg",
           roles: [
-         { charName: "", animeTitle: "", animeId: "", charImage: "" } ] },
+        ] },
 
 
     "𝕾𝖆𝖓𝖋𝖔𝖗𝖆": { 
         name: "𝕾𝖆𝖓𝖋𝖔𝖗𝖆",
          role: "مؤدي أصوات ", 
-         logo: "",
+         logo: "https://files.catbox.moe/e27jgf.jpeg",
           roles: [
          { charName: "", animeTitle: "", animeId: "", charImage: "" } ] },
     "ebra..:)": {
         name: "ebra..:)",
         role: "مؤدي أصوات",
-        logo: "https://p19-common-sign.tiktokcdn-us.com/tos-alisg-avt-0068/4758bb96343e7da688a7615b8d25014e~tplv-tiktokx-cropcenter:1080:1080.webp?dr=9640&refresh_token=9ce535fc&x-expires=1784829600&x-signature=z5UQ77jIYaNInjD%2BA7uycQf8OGQ%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=2472a6c6&idc=useast8",
-        roles: [{ charName: "", animeTitle: "", animeId: "", charImage: "" }] // يمكنك لاحقاً أتمتة إضافة الأدوار هنا
+        logo: "https://files.catbox.moe/8ayfhm.webp",
+        roles: [{ charName: "Flagon Darlett", animeTitle: "Attack on Titan: No Regrets", animeId: "Attack on Titan: No Regrets", charImage: "https://cdn.myanimelist.net/images/characters/4/331901.jpg" }] // يمكنك لاحقاً أتمتة إضافة الأدوار هنا
     },
+
+
+
+
+
+
+
+    // لكلتسكي 
     "توكيتو": {
         name: "توكيتو",
         role: "مؤدي أصوات / قائد",
-        logo: "https://yt3.googleusercontent.com/QM397mtex9n0xWvPgM-yd2jrZdlJNhnl-jEoRAhLzZjlSfEH6uGD54K9nf5I7ufAkIf505Cj=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        logo: "https://files.catbox.moe/bijb9i",
         roles: [{ charName: "", animeTitle: "", animeId: "", charImage: "" }] // يمكنك لاحقاً أتمتة إضافة الأدوار هنا
     },
          
